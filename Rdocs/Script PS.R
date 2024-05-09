6a6cfff1cfb25f1205dfb5b99b55a7bea6011eff
MPMassaro <murilomassaro03@gmail.com>
2024-05-09 02:26:23 -0300

    .

--- Rdocs/Script PS.R
+++ Rdocs/Script PS.R
@@ -30,6 +30,85 @@ estat_theme <- function(...) {
   )
 }
 
+
+print_quadro_resumo <- function(data, title="Medidas resumo da(o) [nome da variável]", label="quad:quadro_resumo1")
+{
+  data <- data %>%
+    summarize(`Média` = round(mean(displ),2),
+              `Desvio Padrão` = round(sd(displ),2),
+              `Variância` = round(var(displ),2),
+              `Mínimo` = round(min(displ),2),
+              `1º Quartil` = round(quantile(displ, probs = .25),2),
+              `Mediana` = round(quantile(displ, probs = .5),2),
+              `3º Quartil` = round(quantile(displ, probs = .75),2),
+              `Máximo` = round(max(displ),2)) %>%
+    t() %>% 
+    as.data.frame() %>%
+    rownames_to_column()
+  
+  latex <- str_c("\\begin{quadro}[H]
+\t\\caption{", title, "}
+\t\\centering
+\t\\begin{adjustbox}{max width=\\textwidth}
+\t\\begin{tabular}{", sep="")
+  
+  col_count <- ncol(data)
+  row_count <- nrow(data)
+  latex <- str_c(latex, "| l |\n", sep=" ")
+  for (i in seq(2, col_count))
+  {
+    numCount <- data[i, -c(1)] %>%
+      as.numeric() %>%
+      {floor(log10(.)) + 1} %>%
+      max()
+    latex <- str_c(latex, "\t\t\tS[table-format = ", numCount ,".2]\n", sep="")
+  }
+  
+  
+  latex <- str_c(latex, "\t\t\t|}\n\t\\toprule\n\t\t", sep="")
+  if (col_count > 2)
+  {
+    for (i in seq(1,col_count))
+    {
+      if (i == 1)
+        latex <- str_c(latex, "\\textbf{Estatística}", sep="")
+      else
+        latex <- str_c(latex, " \\textbf{", data[1, i], "}", sep="")
+      
+      if (i < col_count)
+        latex <- str_c(latex, "&", sep=" ")
+      else
+        latex <- str_c(latex, "\\\\\n", sep=" ")
+    }
+  }
+  else
+  {
+    latex <- str_c(latex, "\\textbf{Estatística} & \\textbf{Valor} \\\\\n", sep="")  
+  }
+  
+  latex <- str_c(latex, "\t\t\\midrule\n", sep="")
+  
+  if (col_count > 2)
+    starting_number <- 2
+  else
+    starting_number <- 1
+  
+  for (i in seq(starting_number, row_count))
+  {
+    latex <- str_c(latex, "\t\t", str_flatten(t(data[i,]), collapse = " & "), " \\\\\n")
+  }
+  latex <- str_c(latex, "\t\\bottomrule
+\t\\end{tabular}
+\t\\label{", label, "}
+\t\\end{adjustbox}
+\\end{quadro}", sep="")
+  
+  writeLines(latex)
+}
+
+
+
+
 df=read_csv("Banco\\Scooby.csv")
 df=select(df,-c(1,2,3))
 df$format=fct_infreq(df$format)
@@ -115,7 +194,9 @@ leveneTest(imdb~season,data=filter(df,season %in% c("1","2","3","4")))
 df %>%
   count(season)
 
-
+df %>%
+  group_by(season) %>%
+  print_quadro_resumo()
 
 
 
